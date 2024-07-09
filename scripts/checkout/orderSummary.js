5474a9631e2b473498d6a3a6ecbc8447fd7cb540
Abduvokh1D <abduvokhideurope@gmail.com>
2024-07-09 17:55:06 +0200

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -12,10 +12,8 @@ import {
   deliveryOptions,
   getDeliveryOption,
 } from "../../data/deliveryOptions.js";
-hello();
-const today = dayjs();
-const deliveryDate = today.add(7, "day");
-console.log(deliveryDate.format("dddd, MMMM D"));
+
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 export function renderOrderSummary() {
   let cartSummaryHTML = "";
@@ -131,6 +129,8 @@ export function renderOrderSummary() {
       );
       container.remove();
       updateCartQuantity();
+
+      renderPaymentSummary();
     });
   });
 
@@ -139,6 +139,7 @@ export function renderOrderSummary() {
       const { productId, deliveryOptionId } = element.dataset;
       updateDeliveryOption(productId, deliveryOptionId);
       renderOrderSummary();
+      renderPaymentSummary();
     });
   });
 
